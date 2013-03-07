7f81b83a28d12cb36f1065ef5c99e31112836846
mustafa dikici <mus.dikici@gmail.com>
2013-03-07 14:50:30 +0200

    Handlebars module added, gitignore created for builded files, app.js and package.json creation process implemented

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,10 @@ module.exports = function(grunt) {
         syndicate : {
             "models" : ["public/models/*"],
             "collections" : ["public/collections/*"],
-            'dest' : ["dest/"],
-
+            'dest' : "dist",
+            'port' : 9080,
+            'name' : 'Syndicate Test App',
+            'version': '1'
         }
     });
 
@@ -14,18 +16,59 @@ module.exports = function(grunt) {
             return grunt.config("syndicate." + c);
         }
 
+        var Handlebars = require('Handlebars');
+        var FILE_ENCODING = 'utf-8';
+
+        /* create folder if not exists */
+        var dest = conf('dest') || 'dist';
+        if(!grunt.file.isDir(dest)){
+            grunt.file.mkdir(dest);
+        }
+        
+        /* generate app.js file */
+        var appJSTemplate = grunt.file.read("templates/appJSTemplate.tmpl", {encoding: FILE_ENCODING});
+        var port = 8080;
+        if(conf("port")) {
+            port = conf("port");
+        }
+        var appJSData = {port: port};
+        var appJSResult = Handlebars.compile(appJSTemplate)(appJSData);
+        grunt.file.write(conf("dest")+"/app.js", appJSResult, {encoding: FILE_ENCODING});
+        console.log("app.js file generated");
+        /* app.js file generated */
+
+        /* generate package.json file */
+        var packageJsonTemplate = grunt.file.read("templates/packageJsonTemplate.tmpl", {encoding: FILE_ENCODING});
+        var name = conf('name') || 'Syndicator Generated App';
+        var version = conf('version') || '0.1.0';
+        var packageJsonData = {name: name, version: version};
+        var packageJsonResult = Handlebars.compile(packageJsonTemplate)(packageJsonData);
+        grunt.file.write(conf("dest")+"/package.json", packageJsonResult, {encoding: FILE_ENCODING});    
+        console.log("package.json file generated");        
+        /* package.json file generated */
+
+/*        var appJSData = {port:port};
+        var appJSResult = Handlebars.compile(ft)
         var modelFiles = grunt.file.expand(conf('models'));
         var collectionFiles = grunt.file.expand(conf("collections"));
 
         grunt.util._.each(modelFiles,function(modelFile){
+
             file = grunt.file.read(modelFile);
             console.log('file is :', modelFile);
             var url = file.match(/url\s*\:\s*[\',\"](.*)[\',\"]\s*,/)[1] || "";
             console.log("url is :", url);
-            grunt.file.write(conf("dest"), url);
+            // grunt.file.write(conf("dest"), url);
         });
 
 
+        
+        var ftt = Handlebars.compile(ft);
+        var data = {name: "Ali", shit: "Cüneyt"};
+        var result = ftt(data);
+
+        grunt.file.write(conf("dest")+"/firstshit", result, {encoding: FILE_ENCODING});
+        console.log("file:", result);*/
     });
 
     grunt.registerTask('default', 'syndicate');
